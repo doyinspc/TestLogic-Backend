36448ae5e044129540c1ce279140064555913ff1
Adedoyin Charles <doyinspc2@gmail.com>
2020-03-04 00:36:40 +0100

    ENV EDIT

--- config/index.js
+++ config/index.js
@@ -4,15 +4,15 @@ require('dotenv').config({
   });
   console.log(process.env.NODE_ENV)
 var mysqlConnection = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user:process.env.DATABASE_USER,
-    password:process.env.DATABASE_PASSWORD,
-    database:process.env.DATABASE_NAME
+    host: process.env.RDS_HOSTNAME,
+    user:process.env.RDS_USERNAME,
+    password:process.env.RDS_PASSWORD,
+    database:process.env.RDS_DB_NAME
 });
 
 mysqlConnection.connect((err)=>{
     if(!err){
-        console.log(`Database Connected to -- ${process.env.DATABASE_PASSWORD} ${process.env.DATABASE_NAME} ${process.env.DATABASE_USER} ${process.env.DATABASE_HOST}`);
+        console.log(`Database Connected to -- ${process.env.RDS_PASSWORD} ${process.env.RDS_DB_NAME} ${process.env.RDS_USERNAME} ${process.env.RDS_HOSTNAME}`);
     }else{
         console.log(`Database Connection Failed ${JSON.stringify(err.sqlMessage, undefined, 2)}`);
     }
